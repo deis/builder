ecdb5b10dddd8e6b66f636b82a12ede792049042
Aaron Schlesinger <arschles@gmail.com>
2016-01-06 15:36:39 -0800

    fix(config.go): specify defaults for server configs

--- pkg/sshd/config.go
+++ pkg/sshd/config.go
@@ -1,7 +1,7 @@
 package sshd
 
 type Config struct {
-	FetcherPort int    `envconfig:"fetcher_port"`
-	SSHHostIP   string `envconfig:"ssh_host_ip"`
-	SSHHostPort int    `envconfig:"ssh_host_port"`
+	FetcherPort int    `envconfig:"fetcher_port" default:"3000"`
+	SSHHostIP   string `envconfig:"ssh_host_ip" default:"0.0.0.0"`
+	SSHHostPort int    `envconfig:"ssh_host_port" default:"2223"`
 }
