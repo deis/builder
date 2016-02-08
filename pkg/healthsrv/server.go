21ad624777b8963c3ab84b1ac5b006a223ebb4be
Aaron Schlesinger <aschlesinger@deis.com>
2016-02-08 11:43:04 -0800

    fix(pkg/healthsrv/server.go): only access port
    assume the health server will server on :$PORT

--- pkg/healthsrv/server.go
+++ pkg/healthsrv/server.go
@@ -13,10 +13,10 @@ const (
 )
 
 // Start starts the healthcheck server on $host:$port and blocks. It only returns if the server fails, with the indicative error
-func Start(host string, port int, s3Client *s3.S3) error {
+func Start(port int, s3Client *s3.S3) error {
 	mux := http.NewServeMux()
 	mux.Handle("/healthz", healthZHandler(s3Client))
 
-	hostStr := fmt.Sprintf("%s:%d", host, port)
+	hostStr := fmt.Sprintf("%d", port)
 	return http.ListenAndServe(hostStr, mux)
 }
