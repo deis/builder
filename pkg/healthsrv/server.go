2b8fc3b034b20719445e2c94c1e05dd7900fb6b1
Aaron Schlesinger <aschlesinger@deis.com>
2016-02-11 16:19:49 -0800

    fix(pkg/healthsrv/server.go): remove unused constants

--- pkg/healthsrv/server.go
+++ pkg/healthsrv/server.go
@@ -7,12 +7,7 @@ import (
 	"github.com/deis/builder/pkg/sshd"
 )
 
-const (
-	DefaultHost = "0.0.0.0"
-	DefaultPort = 8082
-)
-
-// Start starts the healthcheck server on $host:$port and blocks. It only returns if the server fails, with the indicative error
+// Start starts the healthcheck server on :$port and blocks. It only returns if the server fails, with the indicative error
 func Start(port int, nsLister NamespaceLister, bLister BucketLister, sshServerCircuit *sshd.Circuit) error {
 	mux := http.NewServeMux()
 	mux.Handle("/healthz", healthZHandler(nsLister, bLister, sshServerCircuit))
