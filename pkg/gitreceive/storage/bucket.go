2fc00e480c1ee04efa53cf576cc95e9e97c5e0ff
Aaron Schlesinger <aschlesinger@deis.com>
2016-02-16 13:49:45 -0800

    fix(pkg/gitreceive/storage/bucket.go): remove unused func

--- pkg/gitreceive/storage/bucket.go
+++ pkg/gitreceive/storage/bucket.go
@@ -15,23 +15,6 @@ var (
 	ACLPublicRead = aws.String("public-read")
 )
 
-// BucketExists returns if a bucket exists in the S3 API compatible storage
-func BucketExists(svc *s3.S3, bucketName string) (bool, error) {
-	_, err := svc.HeadBucket(&s3.HeadBucketInput{
-		Bucket: aws.String(bucketName),
-	})
-	if err != nil {
-		if awsErr, ok := err.(awserr.Error); ok {
-			if awsErr.Code() == "404" {
-				return false, nil
-			}
-		}
-
-		return false, err
-	}
-	return true, nil
-}
-
 // CreateBucket creates a new bucket in the S3 API compatible storage or
 // return an error in case the bucket already exists
 func CreateBucket(svc *s3.S3, bucketName string) error {
