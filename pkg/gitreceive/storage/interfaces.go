c7f11bde40f8581253bd4095bc2143f40d9ca73a
Aaron Schlesinger <aschlesinger@deis.com>
2016-03-02 15:54:10 -0800

    fix(pkg/gitreceive/storage/interfaces.go): keeping track of calls in the mock interface implementations

--- pkg/gitreceive/storage/interfaces.go
+++ pkg/gitreceive/storage/interfaces.go
@@ -11,12 +11,22 @@ type BucketCreator interface {
 	MakeBucket(bucketName string, acl s3.BucketACL, location string) error
 }
 
+type FakeMakeBucketCall struct {
+	BucketName string
+	ACL        s3.BucketACL
+	Location   string
+}
+
 // FakeBucketCreator is a mock function that can be swapped in for an BucketCreator, so you can unit test your code
-type FakeBucketCreator func(string, s3.BucketACL, string) error
+type FakeBucketCreator struct {
+	Fn    func(string, s3.BucketACL, string) error
+	Calls []FakeMakeBucketCall
+}
 
 // PutObject is the interface definition
-func (f FakeBucketCreator) MakeBucket(name string, acl s3.BucketACL, location string) error {
-	return f(name, acl, location)
+func (f *FakeBucketCreator) MakeBucket(name string, acl s3.BucketACL, location string) error {
+	f.Calls = append(f.Calls, FakeMakeBucketCall{BucketName: name, ACL: acl, Location: location})
+	return f.Fn(name, acl, location)
 }
 
 // ObjectStatter is a *(github.com/minio/minio-go).Client compatible interface, restricted to just the StatObject function. You can use it in your code for easier unit testing without any external dependencies
@@ -24,12 +34,21 @@ type ObjectStatter interface {
 	StatObject(bucketName, objectKey string) (s3.ObjectInfo, error)
 }
 
+type FakeStatObjectCall struct {
+	BucketName string
+	ObjectKey  string
+}
+
 // FakeObjectStatter is a mock function that can be swapped in for an ObjectStatter, so you can unit test your code
-type FakeObjectStatter func(string, string) (s3.ObjectInfo, error)
+type FakeObjectStatter struct {
+	Fn    func(string, string) (s3.ObjectInfo, error)
+	Calls []FakeStatObjectCall
+}
 
 // PutObject is the interface definition
-func (f FakeObjectStatter) StatObject(bucketName, objectKey string) (s3.ObjectInfo, error) {
-	return f(bucketName, objectKey)
+func (f *FakeObjectStatter) StatObject(bucketName, objectKey string) (s3.ObjectInfo, error) {
+	f.Calls = append(f.Calls, FakeStatObjectCall{BucketName: bucketName, ObjectKey: objectKey})
+	return f.Fn(bucketName, objectKey)
 }
 
 // ObjectPutter is a *(github.com/minio/minio-go).Client compatible interface, restricted to just the PutObject function. You can use it in your code for easier unit testing without any external dependencies
@@ -37,10 +56,26 @@ type ObjectPutter interface {
 	PutObject(bucketName, objectKey string, reader io.Reader, contentType string) (int64, error)
 }
 
+type FakePutObjectCall struct {
+	BucketName  string
+	ObjectKey   string
+	Reader      io.Reader
+	ContentType string
+}
+
 // FakeObjectPutter is a mock function that can be swapped in for an ObjectPutter, so you can unit test your code
-type FakeObjectPutter func(bucketName, objectKey string, reader io.Reader, contentType string) (int64, error)
+type FakeObjectPutter struct {
+	Fn    func(bucketName, objectKey string, reader io.Reader, contentType string) (int64, error)
+	Calls []FakePutObjectCall
+}
 
 // PutObject is the interface definition
-func (f FakeObjectPutter) PutObject(bucketName, objectKey string, reader io.Reader, contentType string) (int64, error) {
-	return f(bucketName, objectKey, reader, contentType)
+func (f *FakeObjectPutter) PutObject(bucketName, objectKey string, reader io.Reader, contentType string) (int64, error) {
+	f.Calls = append(f.Calls, FakePutObjectCall{
+		BucketName:  bucketName,
+		ObjectKey:   objectKey,
+		Reader:      reader,
+		ContentType: contentType,
+	})
+	return f.Fn(bucketName, objectKey, reader, contentType)
 }
