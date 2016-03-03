81cd21f4de02b24edef9e44dc5a4bfe992c7c401
Aaron Schlesinger <aschlesinger@deis.com>
2016-03-03 09:55:56 -0800

    fix(pkg/gitreceive/storage/auth_test.go): add preliminary tests for getAuth

---
+++ pkg/gitreceive/storage/auth_test.go
@@ -0,0 +1,32 @@
+package storage
+
+import (
+	"testing"
+
+	"github.com/arschles/assert"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/deis/builder/pkg/sys"
+)
+
+func TestGetAuthEmptyAuth(t *testing.T) {
+	fs := sys.NewFakeFS()
+	creds, err := getAuth(fs)
+	assert.NoErr(t, err)
+	assert.Equal(t, creds, credentials.AnonymousCredentials, "returned credentials")
+}
+
+func TestGetAuthMissingKey(t *testing.T) {
+	fs := sys.NewFakeFS()
+	fs.Files[accessSecretKeyFile] = []byte("hello world")
+	creds, err := getAuth(fs)
+	assert.Err(t, err, errMissingKey)
+	assert.True(t, creds == nil, "returned credentials were not nil")
+}
+
+func TestGetAuthMissingSecret(t *testing.T) {
+	fs := sys.NewFakeFS()
+	fs.Files[accessKeyIDFile] = []byte("hello world")
+	creds, err := getAuth(fs)
+	assert.Err(t, err, errMissingSecret)
+	assert.True(t, creds == nil, "returned credentials were not nil")
+}
