eab6596fc071766cf259c38ab01bdcdf324ac0d2
Aaron Schlesinger <aschlesinger@deis.com>
2016-03-02 16:04:13 -0800

    fix(pkg/gitrecevive/storage/object_test.go): add tests for UploadObject

--- pkg/gitreceive/storage/object_test.go
+++ pkg/gitreceive/storage/object_test.go
@@ -2,6 +2,8 @@ package storage
 
 import (
 	"errors"
+	"io"
+	"strings"
 	"testing"
 
 	"github.com/arschles/assert"
@@ -49,4 +51,30 @@ func TestObjectExistsOtherErr(t *testing.T) {
 	exists, err := ObjectExists(statter, bucketName, objKey)
 	assert.Err(t, err, expectedErr)
 	assert.False(t, exists, "object found when the statter errored")
+}
+
+func TestUploadObjectSuccess(t *testing.T) {
+	rdr := strings.NewReader("hello world!")
+	putter := &FakeObjectPutter{
+		Fn: func(string, string, io.Reader, string) (int64, error) {
+			return 0, nil
+		},
+	}
+	assert.NoErr(t, UploadObject(putter, bucketName, objKey, rdr))
+	assert.Equal(t, len(putter.Calls), 1, "number of calls to PutObject")
+	assert.Equal(t, putter.Calls[0].BucketName, bucketName, "the bucket name")
+	assert.Equal(t, putter.Calls[0].ObjectKey, objKey, "the object key")
+	assert.Equal(t, putter.Calls[0].ContentType, octetStream, "the content type")
+}
+
+func TestUploadObjectFailure(t *testing.T) {
+	rdr := strings.NewReader("hello world")
+	err := errors.New("test error")
+	putter := &FakeObjectPutter{
+		Fn: func(string, string, io.Reader, string) (int64, error) {
+			return 0, err
+		},
+	}
+	assert.Err(t, UploadObject(putter, bucketName, objKey, rdr), err)
+	assert.Equal(t, len(putter.Calls), 1, "number of calls to PutObject")
 }
