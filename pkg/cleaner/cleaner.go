8e9de0134b47b8a0c2ac67877c022ece859f94aa
Keerthan Mala <kmala@engineyard.com>
2016-04-26 14:43:39 -0600

    fix(cleaner): remove the usage of watch api

--- pkg/cleaner/cleaner.go
+++ pkg/cleaner/cleaner.go
@@ -3,46 +3,110 @@
 package cleaner
 
 import (
+	"io/ioutil"
 	"log"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"k8s.io/kubernetes/pkg/api"
 
 	"github.com/deis/builder/pkg/k8s"
 	"github.com/deis/builder/pkg/sys"
+	"k8s.io/kubernetes/pkg/fields"
+	"k8s.io/kubernetes/pkg/labels"
 )
 
 const (
 	dotGitSuffix = ".git"
 )
 
-// Run starts the deleted app cleaner. This function listens to the Kubernetes event stream for
-// events that indicate namespaces that were `DELETED`.
-// If the namespace name matches a folder on the local filesystem, this func deletes that folder.
-// Note that this function blocks until the watcher returned by `nsLister.Watch` is closed, so
-// you should launch it in a goroutine.
-func Run(gitHome string, nsLister k8s.NamespaceWatcher, fs sys.FS) error {
-	watcher, err := nsLister.Watch(nil, nil, "")
+// localDirs returns all of the local directories immediately under gitHome that filter returns true for.
+// filter will receive only the names of each of the top level directories (not their fully qualified paths), and should return true if it should be included in the output
+func localDirs(gitHome string, filter func(string) bool) ([]string, error) {
+	fileInfos, err := ioutil.ReadDir(gitHome)
 	if err != nil {
-		log.Printf("unable to get watch events (%s)", err)
+		return nil, err
 	}
-	for {
-		select {
-		case event, ok := <-watcher.ResultChan():
-			if !ok {
-				break
-			}
-			if event.Type == "DELETED" {
-				switch event.Object.(type) {
-				case (*api.Namespace):
-					namespace := event.Object.(*api.Namespace)
-					appToDelete := gitHome + "/" + namespace.ObjectMeta.Name + dotGitSuffix
-					if err := fs.RemoveAll(appToDelete); err != nil {
-						log.Printf("Cleaner error removing deleted app %s (%s)", appToDelete, err)
-					}
-				default:
-				}
-			}
+	var ret []string
+	for _, fileInfo := range fileInfos {
+		nm := fileInfo.Name()
+		if len(nm) <= 0 || nm == "." || !fileInfo.IsDir() {
+			continue
+		}
+		if filter(nm) {
+			ret = append(ret, nm)
+		}
+	}
+	return ret, nil
+}
+
+// getDiff gets the directories that are not in namespaceList
+func getDiff(namespaceList []api.Namespace, dirs []string) []string {
+	var ret []string
+
+	// create a set of lowercase namespace names
+	namespacesSet := make(map[string]struct{})
+	for _, ns := range namespaceList {
+		lowerName := strings.ToLower(ns.Name)
+		namespacesSet[lowerName] = struct{}{}
+	}
+
+	// get dirs not in the namespaces set
+	for _, dir := range dirs {
+		lowerName := strings.ToLower(dir)
+		if _, ok := namespacesSet[lowerName]; !ok {
+			ret = append(ret, lowerName)
 		}
 	}
 
+	return ret
+}
+
+func stripSuffixes(strs []string, suffix string) []string {
+	ret := make([]string, len(strs))
+	for i, str := range strs {
+		idx := strings.LastIndex(str, suffix)
+		if idx >= 0 {
+			ret[i] = str[:idx]
+		} else {
+			ret[i] = str
+		}
+	}
+	return ret
+}
+
+func dirHasGitSuffix(dir string) bool {
+	return strings.HasSuffix(dir, dotGitSuffix)
+}
+
+// Run starts the deleted app cleaner. Every pollSleepDuration, it compares the result of nsLister.List with the directories in the top level of gitHome on the local file system.
+// On any error, it uses log messages to output a human readable description of what happened.
+func Run(gitHome string, nsLister k8s.NamespaceLister, fs sys.FS, pollSleepDuration time.Duration) error {
+	for {
+		nsList, err := nsLister.List(labels.Everything(), fields.Everything())
+		if err != nil {
+			log.Printf("Cleaner error listing namespaces (%s)", err)
+			continue
+		}
+
+		gitDirs, err := localDirs(gitHome, dirHasGitSuffix)
+		if err != nil {
+			log.Printf("Cleaner error listing local git directories (%s)", err)
+			continue
+		}
+
+		gitDirs = stripSuffixes(gitDirs, dotGitSuffix)
+
+		appsToDelete := getDiff(nsList.Items, gitDirs)
+
+		for _, appToDelete := range appsToDelete {
+			dirToDelete := filepath.Join(gitHome, appToDelete+dotGitSuffix)
+			if err := fs.RemoveAll(dirToDelete); err != nil {
+				log.Printf("Cleaner error removing deleted app %s (%s)", dirToDelete, err)
+			}
+		}
+
+		time.Sleep(pollSleepDuration)
+	}
 }
