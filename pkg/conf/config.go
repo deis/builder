2a6f43461ece18739229aa343f9f32ab2e853281
Matthew Fisher <mfisher@deis.com>
2016-07-06 09:56:07 -0700

    fix(pkg/conf): skip file if directory
    The following code assumes that the file is a file, not a directory.

--- pkg/conf/config.go
+++ pkg/conf/config.go
@@ -51,6 +51,9 @@ func GetStorageParams(env sys.Env) (Parameters, error) {
 	}
 
 	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
 		data, err := ioutil.ReadFile(storageCredLocation + file.Name())
 		if err != nil {
 			return nil, err
